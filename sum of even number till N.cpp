b72d29043011ab959d347ff8957be3c276265972
Kunal kumar <70257564+KUNAL8271@users.noreply.github.com>
2021-02-20 10:57:13 +0530

    Add files via upload

---
+++ sum of even number till N.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    int i=1;
+    int sum=0;
+    cin>>n;
+    while(i<=n){
+        if(i%2==0){
+        sum=sum+i;
+        }i++;
+    }cout<<sum;
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+
+}
+
+

b7b8dcedaa68bf0a5ca7f5e7344eb56e9fd6c2df
Kunal kumar <70257564+KUNAL8271@users.noreply.github.com>
2021-02-20 11:02:40 +0530

    Add files via upload

---
+++ number pattern1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+cin>>n;
+int i=1;
+int k=0;
+while(i<=n){
+int j=1;
+while(j<=i){
+cout<<k+j;
+j=j+1;
+}
+k=k+1;
+cout<<endl;
+i=i+1;}
+
+
+
+}
